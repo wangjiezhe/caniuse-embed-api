8cf16869c9a16b6ca833314d2e6061bde7644fdf
wangjiezhe <wangjiezhe@gmail.com>
2025-10-20 16:14:03 +0800

    ✨ feat(server): add cors middleware for handling cross-origin requests

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
 
 import getFeatureList from "./modules/get-feature-list.js";
@@ -15,11 +16,7 @@ app.set('port', (process.env.PORT || 3000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome to the CanIUse Embed API!");
